69bc479ca879fe1c2ca06b2e7f9ce347124779cc
johanstevencrack <johanstevencrack@gmail.com>
2024-06-22 15:03:50 +0000

    Solve First, Second and Third Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let wordToRepeat = string;
+    if(num !== 0 && num > 0){
+        for(i = 1; i < num; i++) {
+            wordToRepeat += string;
+        }
+        return (wordToRepeat)
+    }
+    else if(num < 0){
+        return ("ERROR");
+    }
+    else{
+        return '';
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("").reverse().join("")
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
