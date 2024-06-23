23a68131087136e4fdbb6799ed5288c83f608419
johanstevencrack <johanstevencrack@gmail.com>
2024-06-23 18:04:00 +0000

    Solve 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(initial, final) {
+    let sum = 0
+    while (typeof initial !== 'number' || typeof final !== 'number') {
+        return 'ERROR'
+    }
+    if(initial > 0 && initial < final){
+        for (i = initial; i <= final; i++) {
+            sum += i
+        }
+    }
+    else if (final > 0 && initial > final){
+        for (i = initial; i >= final; i--) {
+            sum += i
+        }
+    }
+    else{
+        return 'ERROR'
+    }
+    return sum
 };
 
 // Do not edit below this line
