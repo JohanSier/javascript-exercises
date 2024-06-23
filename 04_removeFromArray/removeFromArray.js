dd573c1b3e699cb282e93e8c8e2544ea1ab6cec4
johanstevencrack <johanstevencrack@gmail.com>
2024-06-23 17:21:14 +0000

    Solve 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...itemsToRemove) {
+    // itemsToRemove will now be an array of all arguments passed after 'array'
+    for (let item of itemsToRemove) {
+      let indexToRemove = array.indexOf(item);
+      while (indexToRemove !== -1) { // Keep removing the item as long as it exists in the array
+        array.splice(indexToRemove, 1);
+        indexToRemove = array.indexOf(item);
+      }
+    }
+    return array;
+  };
+  
 
-// Do not edit below this line
+
 module.exports = removeFromArray;
