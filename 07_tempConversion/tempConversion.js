26869c024f2e1dc83b60428ceb6af801e6f85a38
johanstevencrack <johanstevencrack@gmail.com>
2024-07-02 15:42:09 +0000

    Finish Exercises 6, 7 and 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let conversion = (fTemp - 32) * (5/9);
+  return Math.round(conversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let second_conversion = (cTemp * (9/5)) + 32;
+  return Math.round(second_conversion * 10) / 10;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbersArray) {
+  if(numbersArray.length !== 0){
+     let res = numbersArray.reduce((accumulator, currentValue ) => {
+      return accumulator + currentValue;
+    }, 0);
+    return res;
+  }
+  else{
+    return 0;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numbersArray) {
+    let resp = numbersArray.reduce((accumulator, currentValue ) => {
+     return accumulator * currentValue;
+   }, 1);
+   return resp;
 };
 
-const power = function() {
-	
+const power = function(num, powerNum) {
+	return num ** powerNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num != 0){
+    let acc = 1;
+    for(let i = 1; i <= num; i++){
+      acc *= i;
+    }
+    return acc;
+  }
+  else{
+    return 1;
+  }
 };
 
 // Do not edit below this line
